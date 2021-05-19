c4271f7180a0d99ad16f445cd5442610c97a9eb9
Lionel Cassar <lionelcassar2501@gmail.com>
2021-05-18 21:35:04 -0300

    home page done, ready for new product page

---
+++ src/components/Producto.vue
@@ -0,0 +1,80 @@
+<template>
+    <div>
+    <v-card
+        class="mx-auto my-12"
+        max-width="300"
+    >
+        <v-img
+        height="150"
+        src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+        ></v-img>
+
+        <v-card-title></v-card-title>
+
+        <v-card-text>
+        <v-row
+            align="center"
+            class="mx-0"
+        >
+            <v-rating
+            :value="4.5"
+            color="amber"
+            dense
+            half-increments
+            readonly
+            size="14"
+            ></v-rating>
+
+            <div class="grey--text ml-4">
+            4.5 (413)
+            </div>
+        </v-row>
+
+        <div class="my-4 subtitle-1">
+            $ • Italian, Cafe
+        </div>
+
+        <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+        </v-card-text>
+
+        <v-divider class="mx-4"></v-divider>
+
+        <v-card-title>Tonight's availability</v-card-title>
+
+        <v-card-text>
+        <v-chip-group
+            active-class="deep-purple accent-4 white--text"
+            column
+        >
+            <v-chip>
+                <v-icon>mdi-thumb-up-outline</v-icon>(3)
+            </v-chip>
+            <v-chip>
+                <v-icon>mdi-thumb-down-outline</v-icon>(3)
+            </v-chip>
+        </v-chip-group>
+        </v-card-text>
+
+        <v-card-actions>
+        <v-btn
+            color="green"
+            text
+        >
+            Editar
+        </v-btn>
+        <v-btn
+            color="red"
+            text
+        >
+            Eliminar
+        </v-btn>
+        </v-card-actions>
+    </v-card>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'producto',
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,30 @@
     <v-card-title>
       Catalogo de productos
     </v-card-title>
-    <v-card-text v-for="(row, producto) in producto" :key="producto">
-
+    <v-card-text>
+    <v-row>
+      <v-col cols="10" md="4" sm="6">
+        <v-text-field
+          label="Buscar producto"
+          :rules="rules"
+          hide-details="auto"
+          color="black"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="2" md="2" sm="6">
+        <v-btn
+          color="green"
+          text
+          class="mt-4"
+          outlined
+        >Nuevo producto</v-btn>
+      </v-col>
+    </v-row>
+    <v-row style="margin-top: -25px">
+      <v-col cols="12" md="4" sm="12" v-for="(row, index) in productos" :key="index">
+        <producto></producto>
+      </v-col>
+    </v-row>
     </v-card-text>
     <v-divider/>
   </v-card>
@@ -12,12 +34,19 @@
 
 <script>
   //import HelloWorld from '../components/HelloWorld'
+  import producto from '../components/Producto';
 
   export default {
-    name: '',
-
-    components: {
-
-    },
+    name: 'Home',
+    components: { producto },
+    data:() => ({
+      productos: [
+        {id:1},
+        {id:2},
+        {id:3},
+        {id:4},
+        {id:5}
+      ]
+    })
   }
 </script>
